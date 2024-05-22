7d44a04f322ea57bf0efea68534269c87e36e757
Athikhom_ra <athikhom_ra@outlook.com>
2024-05-22 15:03:40 +0700

    Skill-checkpoint-question#1-5 NARu

--- question-4.js
+++ question-4.js
@@ -5,3 +5,24 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+//โครงสร้างข้อมูลเป็น Array[{Object}]
+//ให้เขียนโปรแกรมในการหาสินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้า
+//ค้นหาปริมาณที่น้อยที่สุดใน  array
+let minQuantityValue = Infinity;
+let minQuantity = inventory.map((arr) => {
+  // console.log(arr);
+  if (minQuantityValue > arr.quantity) {
+    minQuantityValue = arr.quantity;
+  }
+  return minQuantityValue;
+});
+// console.log(minQuantityValue);
+// เก็บเป็น Oject,Array ใหม่เพื่อเรียกค่ามาแสดงผลลัพธ์
+const minQuantityOfInventory = inventory.filter((arr) => {
+  return arr.quantity <= minQuantityValue;
+});
+// console.log(minQuantityOfInventory);
+//แสดลงผลลัพธ์ สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ <name> ซึ่งมี <quantity> ชิ้น
+console.log(
+  `สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${minQuantityOfInventory[0].name} ซึ่งมี ${minQuantityOfInventory[0].quantity} ชิ้น`
+);
